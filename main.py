4af83da1760e19f1584232d6c1e1b3dfd2127edf
1elasmarjad <jadelasmar4@gmail.com>
2024-06-06 00:09:45 -0400

    info works

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bot import CustomBot
 from config import ConfigurationManager
+from patreon_api import get_all_pledge_data
 
 discord_bot = CustomBot()
 
@@ -7,16 +8,37 @@ discord_bot = CustomBot()
 @discord_bot.event
 async def on_ready():
     print(f"Logged in as {discord_bot.user} ({discord_bot.user.id})")
-    # print(len(await get_all_pledge_data(config.patreon_campaign_id)))
 
-    #
-    # for x in x['data']:
-    #     if x['relationships']['currently_entitled_tiers']['data']:
-    #         import json
-    #         print(json.dumps(x, indent=4))
+    while True:
+        input_data = input("$ ")
 
-    # import json
-    # print(json.dumps(x, indent=4))
+        split_data = input_data.split(" ")
+
+        match split_data[0]:
+            case "exit":
+                await discord_bot.close()
+                exit(0)
+
+            case "info":  # discord info of a user
+                if len(split_data) < 2:
+                    print("Please provide a user ID")
+                    continue
+
+                discord_id = split_data[1]
+
+                patreon_data = await get_all_pledge_data(ConfigurationManager().get_config()["patreon_campaign_id"])
+
+                for pledge in patreon_data:
+                    if pledge.discord_id == discord_id:
+                        print(f"Discord ID: {discord_id}")
+                        print(f"Email: {pledge.email}")
+                        print(f"Patron Status: {pledge.patron_status}")
+                        print(
+                            f"Entitled Tiers: {', '.join([f"{tier.title} {tier.discord_role_ids}" for tier in pledge.entitled_tiers])}")
+                        break
+
+            case _:
+                print("Unknown command")
 
 
 if __name__ == '__main__':
